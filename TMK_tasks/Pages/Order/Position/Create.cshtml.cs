5b52a1b3c719c11fce24f90675f97f37ef1ba903
Maksim <angrybearyt@gmail.com>
2023-03-29 05:14:38 +0500

    rename namespace

--- TMK_tasks/Pages/Order/Position/Create.cshtml.cs
+++ TMK_tasks/Pages/Order/Position/Create.cshtml.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using TMK_tasks.Data;
-using TMK_tasks.Data.Entities;
 
 namespace TMK_tasks.Pages.Order.Position
 {
